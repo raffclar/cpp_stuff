f116630ac3fd37139fd1a5e504a046e8d5d47b74
gavxn <gavxn@zanidrak.com>
2018-05-08 18:23:50 +0100

    Add run_build.sh

---
+++ run_build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+BG='\033[0;32m'
+NC='\033[0m'
+
+error() {
+    >&2 echo -e "${RED}$1${NC}";
+}
+
+info() {
+    echo -e "${BG}$1${NC}";
+}
+
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+
+FLAGS="-j8 -Wall"
+
+make ${FLAGS}
+
+if [ $? -ne 0 ]; then
+    error "Error: failed to compile!"
+    exit 1
+fi
