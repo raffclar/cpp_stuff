3da4c05b13eb172805b27e5363a1ce0d0beca0c5
gavxn <gavxn@zanidrak.com>
2019-05-26 02:11:00 +0100

    Add exit_after_x_seconds

---
+++ exit_after_x_seconds/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main (int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Provide an integer between 0 and 10000. Exiting" << std::endl;
+        return 1;
+    }
+
+    int64_t seconds;
+
+    try {
+        seconds = std::stoi(argv[1]);
+    } catch (const std::invalid_argument &ex) {
+        std::cerr << "The provided argument, \"" << argv[1] << "\", is not an integer." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Sleeping for " << seconds << " seconds." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    std::cout << "Finished." << std::endl;
+    return 0;
+}
